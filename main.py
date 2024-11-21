cae4567756bef9486353a1ae85721bcb523a7c5f
arch_rival <123332908+rustleupchef@users.noreply.github.com>
2024-11-21 13:16:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import cv2 as cv
+import os
 
-hello = cv.CascadeClassifier('cascade/cascade.xml')
-video = cv.VideoCapture(0)
-while cv.waitKey(1) != 27:
-    ret,frame = video.read()
-    grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    rectangles = hello.detectMultiScale(grayFrame, scaleFactor=1.3, minNeighbors=7)
-    if len(rectangles) > 0:
-        for (x, y, w, h) in rectangles:
-            cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
-    cv.imshow("frame", frame)
+hello = cv.CascadeClassifier('Cascades/hello_cascade.xml')
 
-
-video.release()
+if input("Y/N? - ").lower() == "y":
+    video = cv.VideoCapture(0)
+    while cv.waitKey(1) != 27:
+        ret,frame = video.read()
+        grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        rectangles = hello.detectMultiScale(grayFrame)
+        if len(rectangles) > 0:
+            for (x, y, w, h) in rectangles:
+                cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
+        cv.imshow("frame", frame)
+    
+    video.release()
+else:
+    names = os.list.dir("Positives/hello/")
+    i = 0
+    while True:
+        key = cv.waitKey(1)
+        frame = cv.imread(f"Positives/hello/{names[i]}")
+        grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        rectangles = hello.detectMultiScale(grayFrame)
+        if len(rectangles) > 0:
+            for (x, y, w, h) in rectangles:
+                cv.rectangle(frame, (x, y,) (x + w, y + h), (0, 255, 0), thickness=2)
+        cv.imshow("frame", frame)
+        if key == 27: break
+        if key == ord('q'):
+            i = (i + 1) % len(names)
 cv.destroyAllWindows()
